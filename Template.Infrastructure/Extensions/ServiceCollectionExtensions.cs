365bd9ffa6bfe6e0cc3253031fedc1290776e5bf
Loukas998 <majdlouka4@gmail.com>
2025-08-01 18:37:12 +0300

    fixed connection string

--- Template.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Template.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -10,37 +10,37 @@ using Template.Infrastructure.Seeders;
 using Template.Infrastructure.Services;
 
 namespace Template.Infrastructure.Extensions;
-
+//Server=(localdb)\\mssqllocaldb;Database=OuzonDb;Trusted_Connection=True;
 public static class ServiceCollectionExtensions
 {
-	public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
-	{
-		var connectionString = configuration.GetConnectionString("TemplateDb");
-		services.AddDbContext<TemplateDbContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=OuzonDb;Trusted_Connection=True;"));
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("TemplateDb");
+        services.AddDbContext<TemplateDbContext>(options => options.UseSqlServer("Server=ouzondb.mssql.somee.com;Database=ouzondb;User Id=majdlouka_SQLLogin_1;Password=majd2003;TrustServerCertificate=True;MultipleActiveResultSets=True;"));
 
-		//this for identity and jwt when needed
-		services.AddIdentityCore<User>()
-			.AddRoles<IdentityRole>()
-			.AddTokenProvider<DataProtectorTokenProvider<User>>("TemplateTokenProvidor")
-			.AddEntityFrameworkStores<TemplateDbContext>()
-			.AddDefaultTokenProviders();
+        //this for identity and jwt when needed
+        services.AddIdentityCore<User>()
+            .AddRoles<IdentityRole>()
+            .AddTokenProvider<DataProtectorTokenProvider<User>>("TemplateTokenProvidor")
+            .AddEntityFrameworkStores<TemplateDbContext>()
+            .AddDefaultTokenProviders();
         //----------------------------------------------------------------------------------------------------------------
         //services and repositories 
-		services.AddScoped<IRolesSeeder, RolesSeeder>();
-		services.AddScoped<ICategorySeeder, CategorySeeder>();
+        services.AddScoped<IRolesSeeder, RolesSeeder>();
+        services.AddScoped<ICategorySeeder, CategorySeeder>();
 
-		services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         // we use typeof because the interface and the class are generic
         // and without it we would have to specify the type(IGenericRepository<Kit>, GenericType<Kit>)
         services.AddScoped<ITokenRepository, TokenRepository>();
-		services.AddScoped<IDeviceRepository, DeviceRepository>();
+        services.AddScoped<IDeviceRepository, DeviceRepository>();
         services.AddScoped<IAccountRepository, AccountRepository>();
         services.AddScoped<IKitRepository, KitRepository>();
-		services.AddScoped<IImplantRepository, ImplantRepository>();
-		services.AddScoped<IToolRepository, ToolRepository>();
-		services.AddScoped<IProcedureRepository, ProcedureRepository>();
-		services.AddScoped<IHolidayRepository, HolidayRepository>();
-		services.AddScoped<IRatingsRepository, RatingRepository>();
-		services.AddTransient<INotificationService, NotificationService>();
-	}
+        services.AddScoped<IImplantRepository, ImplantRepository>();
+        services.AddScoped<IToolRepository, ToolRepository>();
+        services.AddScoped<IProcedureRepository, ProcedureRepository>();
+        services.AddScoped<IHolidayRepository, HolidayRepository>();
+        services.AddScoped<IRatingsRepository, RatingRepository>();
+        services.AddTransient<INotificationService, NotificationService>();
+    }
 }
